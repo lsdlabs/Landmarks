bbf9d27c0dcde30748c6625317bea3a96ca23e47
Lauren <laurkeizsmall@gmail.com>
2020-07-23 12:01:39 -0400

    first commit

---
+++ Landmarks/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Landmarks
+//
+//  Created by Lauren Small on 7/23/20.
+//  Copyright © 2020 Lauren Small. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, SwiftUI!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
